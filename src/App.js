8b20bd629dfcf9d6242661e04f4dbd4c05e8ef1c
ben roy zybert <96917127+benzybert@users.noreply.github.com>
2024-12-18 18:05:50 +0200

    Update App.js with ToastProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
+import { ToastProvider } from './context/ToastContext';
+import PageTransition from './components/layout/PageTransition';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import Products from './pages/Products';
@@ -8,23 +10,35 @@ import ProductPage from './pages/ProductPage';
 import Cart from './pages/Cart';
 import './App.css';
 
+const AppRoutes = () => {
+  const location = useLocation();
+
+  return (
+    <PageTransition>
+      <Routes location={location}>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductPage />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </PageTransition>
+  );
+};
+
 function App() {
   return (
-    <CartProvider>
-      <Router>
-        <div className="app">
-          <Navbar />
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/products/:id" element={<ProductPage />} />
-              <Route path="/cart" element={<Cart />} />
-            </Routes>
-          </main>
-        </div>
-      </Router>
-    </CartProvider>
+    <ToastProvider>
+      <CartProvider>
+        <Router>
+          <div className="app">
+            <Navbar />
+            <main className="main-content">
+              <AppRoutes />
+            </main>
+          </div>
+        </Router>
+      </CartProvider>
+    </ToastProvider>
   );
 }
 
