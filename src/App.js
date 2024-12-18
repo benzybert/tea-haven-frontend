215cb813d739b1fad20779657557888614f0a30c
ben roy zybert <96917127+benzybert@users.noreply.github.com>
2024-12-18 17:59:03 +0200

    Update App.js to include ProductPage route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import ProductPage from './pages/ProductPage';
 import './App.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/products" element={<Products />} />
+            <Route path="/products/:id" element={<ProductPage />} />
           </Routes>
         </main>
       </div>
