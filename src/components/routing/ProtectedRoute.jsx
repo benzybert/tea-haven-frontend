597511297c966c7fe5ef50feedc44f2356cddb01
ben roy zybert <96917127+benzybert@users.noreply.github.com>
2024-12-19 12:49:49 +0200

    Update ProtectedRoute component

--- src/components/routing/ProtectedRoute.jsx
+++ src/components/routing/ProtectedRoute.jsx
@@ -1,10 +1,14 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-const ProtectedRoute = () => {
+const ProtectedRoute = ({ children }) => {
   const { user } = useSelector((state) => state.auth);
 
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
