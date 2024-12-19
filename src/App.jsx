0b8cc9515dc924b000b19adae222d552c391d3aa
ben roy zybert <96917127+benzybert@users.noreply.github.com>
2024-12-19 12:04:10 +0200

    Update App.jsx with test route

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ import RegisterForm from './components/auth/RegisterForm';
 import ForgotPasswordForm from './components/auth/ForgotPasswordForm';
 import ResetPasswordForm from './components/auth/ResetPasswordForm';
 import ProtectedRoute from './components/routing/ProtectedRoute';
+import TestPage from './components/auth/TestPage';
 
-// Other Components (to be implemented)
+// Layout Components
 import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Home from './components/Home';
@@ -17,30 +18,28 @@ import Profile from './components/Profile';
 
 function App() {
   return (
-    <Provider store={store}>
-      <Router>
-        <div className="min-h-screen flex flex-col">
-          <Header />
-          <main className="flex-grow">
-            <Routes>
-              {/* Public Routes */}
-              <Route path="/" element={<Home />} />
-              <Route path="/login" element={<LoginForm />} />
-              <Route path="/register" element={<RegisterForm />} />
-              <Route path="/forgot-password" element={<ForgotPasswordForm />} />
-              <Route path="/reset-password/:token" element={<ResetPasswordForm />} />
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-grow">
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
+            <Route path="/forgot-password" element={<ForgotPasswordForm />} />
+            <Route path="/reset-password/:token" element={<ResetPasswordForm />} />
+            <Route path="/test" element={<TestPage />} />
 
-              {/* Protected Routes */}
-              <Route element={<ProtectedRoute />}>
-                <Route path="/profile" element={<Profile />} />
-                {/* Add more protected routes here */}
-              </Route>
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-    </Provider>
+            {/* Protected Routes */}
+            <Route element={<ProtectedRoute />}>
+              <Route path="/profile" element={<Profile />} />
+            </Route>
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
