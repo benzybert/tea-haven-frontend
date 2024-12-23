e1536976a5da1b35375b6e8f7292778725b02f74
ben roy zybert <96917127+benzybert@users.noreply.github.com>
2024-12-23 13:01:08 +0200

    fix: complete layout components and add common components

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,1 @@
-export { default as Button } from './Button';
-export { default as Input } from './Input';
-export { default as Card } from './Card';
-export { default as ProtectedRoute } from './ProtectedRoute';
+export { default as LoadingSpinner } from './LoadingSpinner';
